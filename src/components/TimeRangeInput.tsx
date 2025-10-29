4c59b4cfdfb5f10cc105358d6403cad3907a65f1
Yulian <yulianguinand365@gmail.com>
2025-10-29 14:47:56 +0100

    fix: remove date selection

--- src/components/TimeRangeInput.tsx
+++ src/components/TimeRangeInput.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { ChevronDownIcon } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { Button } from "./ui/button";
-import { Calendar } from "./ui/calendar";
 import { Input } from "./ui/input"; // ShadCN UI
 import { Label } from "./ui/label";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 interface TimeRangeInputProps {
   index: number;
@@ -23,17 +20,6 @@ export const TimeRangeInput: React.FC<TimeRangeInputProps> = ({
   onChange,
   onRemove,
 }) => {
-  const [openStart, setOpenStart] = useState(false);
-  const [openEnd, setOpenEnd] = useState(false);
-
-  // États séparés pour la date et l'heure
-  const [dateStart, setDateStart] = useState<Date | undefined>(
-    start ? new Date(start) : undefined
-  );
-  const [dateEnd, setDateEnd] = useState<Date | undefined>(
-    end ? new Date(end) : undefined
-  );
-
   const [timeStart, setTimeStart] = useState(
     start ? new Date(start).toTimeString().slice(0, 5) : ""
   );
@@ -41,10 +27,10 @@ export const TimeRangeInput: React.FC<TimeRangeInputProps> = ({
     end ? new Date(end).toTimeString().slice(0, 5) : ""
   );
 
-  const combineDateTime = (date?: Date, time?: string): string => {
-    if (!date || !time) return "";
+  const combineDateTime = (time?: string): string => {
+    if (!time) return "";
     const [hours, minutes] = time.split(":").map(Number);
-    const fullDate = new Date(date);
+    const fullDate = new Date();
     fullDate.setHours(hours!);
     fullDate.setMinutes(minutes!);
     fullDate.setSeconds(0);
@@ -52,47 +38,19 @@ export const TimeRangeInput: React.FC<TimeRangeInputProps> = ({
   };
 
   useEffect(() => {
-    const combinedStart = combineDateTime(dateStart, timeStart);
+    const combinedStart = combineDateTime(timeStart);
     onChange(index, "start", combinedStart);
-  }, [dateStart, timeStart]);
+  }, [timeStart]);
 
   useEffect(() => {
-    const combinedEnd = combineDateTime(dateEnd, timeEnd);
+    const combinedEnd = combineDateTime(timeEnd);
     onChange(index, "end", combinedEnd);
-  }, [dateEnd, timeEnd]);
+  }, [timeEnd]);
 
   return (
     <div className="flex items-end space-x-2 mb-2">
       {/* Bloc START */}
       <div className="flex gap-4">
-        <div className="flex flex-col gap-3">
-          <Label className="px-1">Date</Label>
-          <Popover open={openStart} onOpenChange={setOpenStart}>
-            <PopoverTrigger asChild>
-              <Button
-                variant="outline"
-                className="w-32 justify-between font-normal"
-              >
-                {dateStart ? dateStart.toLocaleDateString() : "Select date"}
-                <ChevronDownIcon />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent
-              className="w-auto overflow-hidden p-0"
-              align="start"
-            >
-              <Calendar
-                mode="single"
-                selected={dateStart}
-                captionLayout="dropdown"
-                onSelect={(date) => {
-                  setDateStart(date);
-                  setOpenStart(false);
-                }}
-              />
-            </PopoverContent>
-          </Popover>
-        </div>
         <div className="flex flex-col gap-3">
           <Label className="px-1">Time</Label>
           <Input
@@ -108,34 +66,6 @@ export const TimeRangeInput: React.FC<TimeRangeInputProps> = ({
 
       {/* Bloc END */}
       <div className="flex gap-4">
-        <div className="flex flex-col gap-3">
-          <Label className="px-1">Date</Label>
-          <Popover open={openEnd} onOpenChange={setOpenEnd}>
-            <PopoverTrigger asChild>
-              <Button
-                variant="outline"
-                className="w-32 justify-between font-normal"
-              >
-                {dateEnd ? dateEnd.toLocaleDateString() : "Select date"}
-                <ChevronDownIcon />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent
-              className="w-auto overflow-hidden p-0"
-              align="start"
-            >
-              <Calendar
-                mode="single"
-                selected={dateEnd}
-                captionLayout="dropdown"
-                onSelect={(date) => {
-                  setDateEnd(date);
-                  setOpenEnd(false);
-                }}
-              />
-            </PopoverContent>
-          </Popover>
-        </div>
         <div className="flex flex-col gap-3">
           <Label className="px-1">Time</Label>
           <Input
